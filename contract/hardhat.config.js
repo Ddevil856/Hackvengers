82606006deabc1cdb71a4539409c133676830603
Dev Sapariya <devsapariya94@gmail.com>
2023-03-14 10:37:01 +0530

    Migrate from react to flask

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,11 +1,18 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    version: "0.8.9",
+    networks: {
+      hardhat: {},
+      mumbai: {
+        url: "https://rpc.ankr.com/polygon_mumbai",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
       },
     },
   },
