da9e23a42e006e76a5de522781318020115fa823
Dev Sapariya <devsapariya94@gmail.com>
2023-03-14 13:32:36 +0530

    add to retrive the firs done by the user

--- contract/contracts/Contract.sol
+++ contract/contracts/Contract.sol
@@ -1,93 +1,88 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Contract {
+contract FIRContract {
     struct FIR {
-        uint256 FIR_NUMBER;
-        uint256 User_ID;
-        uint256 Date_of_Launch;
-        string Name_of_Launcher;
-        string District;
-        string Police_Station;
-        int Police_Station_Code;
-        string DOB;
-        string Date_of_Crime;
-        string Place_of_Crime;
-        string Description_of_Crime;
+        uint256 firNumber;
+        uint256 userId;
+        uint256 dateOfLaunch;
+        string nameOfLauncher;
+        string district;
+        string policeStation;
+        int256 policeStationCode;
+        string dob;
+        string dateOfCrime;
+        string placeOfCrime;
+        string descriptionOfCrime;
+        string evidence_ID;
         string[] updates;
     }
 
-    struct Suspect {
-        string Name_of_Suspect;
-        uint256 age_of_Suspect;
-    }
-
-    FIR[] private Fir_List;
-    uint256 private FIR_NUMBER;
+    FIR[] private firList;
+    uint256 private firNumber;
 
     constructor() {
-        FIR_NUMBER = 0;
+        firNumber = 0;
     }
 
     function raiseFIR(
-        uint256 dol,
-        uint256 user_id,
+        uint256 userId,
         string memory name,
         string memory district,
-        string memory police_station,
-        int police_station_code,
-        string memory birth_date,
-        string memory date_of_crime,
-        string memory place_of_crime,
-        string memory description
-       
+        string memory policeStation,
+        int256 policeStationCode,
+        string memory birthDate,
+        string memory dateOfCrime,
+        string memory placeOfCrime,
+        string memory description,
+        string memory evidenceID
     ) public returns (uint256) {
-        FIR_NUMBER++;
-        Fir_List.push(
+        firNumber++;
+        firList.push(
             FIR(
-                FIR_NUMBER,
-                dol,
-                user_id,
+                firNumber,
+                userId,
+                block.timestamp,
                 name,
                 district,
-                police_station,
-                police_station_code,
-                birth_date,
-                date_of_crime,
-                place_of_crime,
+                policeStation,
+                policeStationCode,
+                birthDate,
+                dateOfCrime,
+                placeOfCrime,
                 description,
+                evidenceID,
                 new string[](0)
             )
         );
-        return FIR_NUMBER;
+        return firNumber;
     }
 
-    function retrieveFIR(
-        uint256 fir_number
-    ) public view returns (FIR memory a) {
-        require(
-            fir_number > 0 && fir_number <= Fir_List.length,
-            "Invalid FIR Number"
-        );
-        return Fir_List[fir_number - 1];
+    function retrieveFIR(uint256 id) public view returns (FIR memory) {
+        if (id > 0 && id <= firList.length) {
+            return firList[id - 1];
+        } else {
+            revert("Invalid FIR Number");
+        }
     }
 
-    function allPoliceStationFIR(int police_station_code) public view returns (FIR[] memory) 
+    function allPoliceStationFIR(int256 policeStationCode)
+        public
+        view
+        returns (FIR[] memory)
     {
-        require(police_station_code != 0, "Please enter a Police Station Code");
-
         uint256 count = 0;
-        for (uint256 i = 0; i < Fir_List.length; i++) {
-            if (Fir_List[i].Police_Station_Code == police_station_code) {
+        for (uint256 i = 0; i < firList.length; i++) {
+            if (firList[i].policeStationCode == policeStationCode) {
                 count++;
             }
         }
 
         FIR[] memory result = new FIR[](count);
         uint256 j = 0;
-        for (uint256 i = 0; i < Fir_List.length; i++) {
-            if (Fir_List[i].Police_Station_Code == police_station_code) {
-                result[j] = Fir_List[i];
+        for (uint256 i = 0; i < firList.length; i++) {
+            if (firList[i].policeStationCode == policeStationCode) {
+                result[j] = firList[i];
                 j++;
             }
         }
@@ -95,31 +90,29 @@ contract Contract {
         return result;
     }
 
-    function retrievebyuserid(uint256 user_id) public view returns (FIR[] memory) 
+    function retrieveByUserId(uint256 userId)
+        public
+        view
+        returns (FIR[] memory)
     {
-    uint256 count = 0;
-    
-    for (uint256 i = 0; i < Fir_List.length; i++) 
-    {
-        if (Fir_List[i].User_ID == user_id) 
-        {
-            count++;
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < firList.length; i++) {
+            if (firList[i].userId == userId) {
+                count++;
+            }
         }
-    }
 
-    FIR[] memory result = new FIR[](count);
-    uint256 j = 0;
+        FIR[] memory result = new FIR[](count);
+        uint256 j = 0;
 
-    for (uint256 i = 0; i < Fir_List.length; i++) 
-    {
-        if (Fir_List[i].User_ID == user_id) 
-        {
-            result[j] = Fir_List[i];
-            j++;
+        for (uint256 i = 0; i < firList.length; i++) {
+            if (firList[i].userId == userId) {
+                result[j] = firList[i];
+                j++;
+            }
         }
-    }
 
-    return result;
+        return result;
     }
-
 }
